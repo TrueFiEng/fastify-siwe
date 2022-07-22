82d979bc25a091e69f07e80b4c60e65a7abacdef
Jakub Sieczczyński <75097304+jakvbs@users.noreply.github.com>
2022-07-22 09:02:52 +0200

    Update packages/fastify-siwe/src/registerSiweRoutes.ts
    Co-authored-by: Przemyslaw Rzad <roopert7@gmail.com>

--- packages/fastify-siwe/src/registerSiweRoutes.ts
+++ packages/fastify-siwe/src/registerSiweRoutes.ts
@@ -39,7 +39,7 @@ export const registerSiweRoutes = (fastify: FastifyInstance, { store }: Register
       reply
         .setCookie('authToken', authToken, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
+          secure: process.env.NODE_ENV !== 'development',
           sameSite: 'strict',
           maxAge: 1000 * 60 * 60 * 24,
           path: '/',
